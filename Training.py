597f4dcb77d4625908ffaadfb4347898d6d37faa
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2020-04-25 18:39:16 +0200

    small cleaning

--- Training.py
+++ Training.py
@@ -302,21 +302,3 @@ if args.epochs < 1:
     sys.exit()
 
 train(model_type,use_attention,args.setup,args.epochs)
-    
-#Snippet might come useful later
-#import tensorflow as tf
-#print(type(tf.__version__))
-#version_tf = float(tf.__version__.split('.')[0])
-#if(version_tf >= 2):
-#  print(history_dict['accuracy'][0])#its because its a list of 1 element
-#  print(history_dict['loss'][0])
-#  print(history_dict['val_accuracy'][0])
-#  print(history_dict['val_loss'][0])
-#else:
-#  print(history_dict['acc'][0])
-#  print(history_dict['loss'][0])
-#  print(history_dict['val_acc'][0])
-#  print(history_dict['val_loss'][0])
-    
-    
-#To do for refactoring:
