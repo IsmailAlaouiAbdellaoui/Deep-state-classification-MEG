61a3751985e6eae6e6ad1aa55df5e0e661c24cde
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2020-05-03 22:04:51 +0200

    handling of negative dimensions

--- MultiviewAttention.py
+++ MultiviewAttention.py
@@ -156,9 +156,13 @@ class MultiviewAttention:
     
     
     def tfaugmented_conv2d(self,X, Fout, k, dk, dv, Nh, relative):
-      conv_out = Conv2D(filters=Fout - dv,kernel_size=k, padding='same')(X)
-      attn_out = self.self_attention_2d(X, dk, dv, Nh, relative=relative)
-      return tf.concat([conv_out, attn_out], axis=3)
+        if Fout - dv < 0:
+            filters = 1
+        else:
+            filters = Fout - dv
+        conv_out = Conv2D(filters=filters,kernel_size=k, padding='same')(X)
+        attn_out = self.self_attention_2d(X, dk, dv, Nh, relative=relative)
+        return tf.concat([conv_out, attn_out], axis=3)
     
     def get_model(self):
 
